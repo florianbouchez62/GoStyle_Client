a610a6f38fa9681ebf6c7add2ac8425cf41a1f48
dylanrousselleaz@gmail.com <dylanrousselleaz@gmail.com>
2020-03-09 10:43:27 +0100

    QRcode

--- App_GoStyle/App/Activity/PromoActivity.js
+++ App_GoStyle/App/Activity/PromoActivity.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import value from '../Activity/QRcodeActivity'
 
-export default function App() {
-  return (
-    <View style={style.container}>
-      <Text>Promo Screen</Text>
-    </View>
-  );
-}
+export default class App extends Component {
+    state = {
+       data: ''
+    }
+    componentDidMount = () => {
+       fetch('http://192.168.42.120:8000/promotions/1',{
+          method: 'GET'
+       })
+       .then((response) => response.json())
+       .then((responseJson) => {
+          console.log(responseJson);
+
+          this.setState({
+             data: responseJson
+          })
+       })
+       .catch((error) => {
+          console.error(error);
+       });
+    }
+    render() {
+       return (
+          <View>
+             <Text>
+                Libelle : {this.state.data.libelle}
+             </Text>
+          </View>
+       )
+    }
+ }
 
 const style = StyleSheet.create({
   container: {
